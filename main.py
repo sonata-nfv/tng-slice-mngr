a056cbe1ad0654234b3b9f432e12392614617177
Felipe Vicens <felipe.vicens@atos.net>
2018-03-19 16:32:15 +0100

    Fix issue changing port to integer

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ def postNSItermination(nsiId):
 
 #MAIN FUNCTION OF THE SERVER
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port='5998')
+  app.run(debug=True, host='0.0.0.0', port=5998)
