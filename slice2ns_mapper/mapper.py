fc23f7893754cf811dce5c738866eb356ef71e9b
Pol Alemany <paprats8@gmail.com>
2018-03-20 16:04:35 +0100

    Fixed typo in README, adding slice2ns_mapper folder

---
+++ slice2ns_mapper/mapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#From José:
+#To deploy (instantiate) the service, use this API (for now) in: https://github.com/sonata-nfv/son-gkeeper/wiki/Requests-usage
+#
+#and follow the next steps:
+#1) login at: https://github.com/sonata-nfv/son-gkeeper/wiki/Authentication-usage#sessions
+#	--> user: sonata / password: 1234
+#
+#2) pass the HTTP Authorization header with "bearer $ACCESS_TOKEN" (returned in the login)
+#
+#3) deploy...
+
+
+
+class mapper:
+    def connect2sonata():
+      return 0
+      
+    def instantiation2sonata():
+      return 0
