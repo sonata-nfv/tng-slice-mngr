439f1247d0209c2473340883965c11bfe317d99f
alemanyp <paprats8@gmail.com>
2018-06-15 13:55:08 +0200

    deleted network-alias parameter from nsiapi.sh and nstapi.sh

--- tests/unit/nsiapi.sh
+++ tests/unit/nsiapi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker run -i \
---network-alias=tng-slice-mngr \
 --rm=true \
 -v "$(pwd)/spec/reports/tng-slice-mngr:/code/log" \
 registry.sonata-nfv.eu:5000/tng-slice-mngr python unit_nst_test.py
