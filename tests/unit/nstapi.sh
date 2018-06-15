ffece40b302c04a4378f6f5f4e6535e823c4d664
alemanyp <paprats8@gmail.com>
2018-06-15 13:47:38 +0200

    updated registry port to call the python unit tests

--- tests/unit/nstapi.sh
+++ tests/unit/nstapi.sh
@@ -4,4 +4,4 @@ docker run -i \
 --network-alias=tng-slice-mngr \
 --rm=true \
 -v "$(pwd)/spec/reports/tng-slice-mngr:/code/log" \
-registry.sonata-nfv.eu:5998/tng-slice-mngr python unit_nst_test.py
+registry.sonata-nfv.eu:5000/tng-slice-mngr python unit_nst_test.py
