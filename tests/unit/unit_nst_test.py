7597a8fe19a2c2bb3b0e441df6ed994cb9228d47
alemanyp <paprats8@gmail.com>
2018-06-19 11:18:49 +0200

    fixing "AttributeError: 'dict' object has no attribute 'status_code'" error -> changed the final assets (no status_code used but text_values)

--- tests/unit/unit_nst_test.py
+++ tests/unit/unit_nst_test.py
@@ -30,30 +30,32 @@ class TestCase(unittest.TestCase):
     @patch('slice_lifecycle_mgr.nst_manager2catalogue.requests.post')
     def test_create_NST(self, mock_createNST):
         #Mock answer for the request to the catalogues     
-        mock_createNST.return_value.status_code = 201
-        mock_createNST.return_value.text = '{"created_at": "2018-06-08T10:36:53.425+00:00","md5": "5024cfde7637ab98f086ff51bd158bc9","nstd": {"author": "5gTango","name": "tango_NST","notificationTypes": "","nstNsdIds": ["6a01afdc-9d42-4bc9-866c-a8a3868fdf5e"],"onboardingState": "ENABLED","operationalState": "ENABLED","usageState": "NOT_IN_USE","userDefinedData": "","vendor": "5gTango","version": "1.0"},"signature": null,"status": "active","updated_at": "2018-06-08T10:36:53.425+00:00","username": null,"uuid": "096c26f9-6142-43d2-8521-57cea9e76c6c"}'
+        #mock_createNST.return_value.status_code = 201
+        mock_createNST.return_value.text = '{"created_at": "2018-06-08T10:36:53.425+00:00","md5": "5024cfde7637ab98f086ff51bd158bc9","nstd": {"author": "5gTango","name": "5gtango_NST_name","notificationTypes": "","nstNsdIds": ["6a01afdc-9d42-4bc9-866c-a8a3868fdf5e"],"onboardingState": "ENABLED","operationalState": "ENABLED","usageState": "NOT_IN_USE","userDefinedData": "","vendor": "5gTango","version": "1.0"},"signature": null,"status": "active","updated_at": "2018-06-08T10:36:53.425+00:00","username": null,"uuid": "096c26f9-6142-43d2-8521-57cea9e76c6c"}'
         
         #Preapres de information received from the protal.
         NetService_1_uuid = str(uuid.uuid4())
         NetService_2_uuid = str(uuid.uuid4())
-        data = {"name":"tango_NST", "version":"1.0", "author":"5gtango", "vendor":"5gTango", "nstNsdIds":[{"NsdId":"52047455-5792-4ce7-8809-2f56c4a876bd"},{"NsdId":"fb050678-2b12-432a-8ee2-a2dff777510f"}]}
+        data = {"name":"5gtango_NST_name", "version":"1.0", "author":"5gtango", "vendor":"5gTango", "nstNsdIds":[{"NsdId":"52047455-5792-4ce7-8809-2f56c4a876bd"},{"NsdId":"fb050678-2b12-432a-8ee2-a2dff777510f"}]}
         
         #Testing the function to create NST
         response = createNST(data)
         
-        self.assertEqual(response.status_code, 201)
-        #resp_json = json.loads(response.data)
+        #self.assertEqual(response.status_code, 201)
         
-#        NST_uuid = resp_json["uuid"]
-#        NST_name = resp_json["nstd"]["name"]
-#        NST_usageState = resp_json["nstd"]["usageState"]
-#        NST_onboardingState = resp_json["nstd"]["onboardingState"]
-#        NST_operationalState = resp_json["nstd"]["operationalState"]
-#        
-#        self.assertEqual(NST_name, "5gtango_NST_name")
-#        self.assertEqual(NST_usageState, "NOT_IN_USE")
-#        self.assertEqual(NST_onboardingState, "ENABLED")
-#        self.assertEqual(NST_operationalState, "ENABLED")
+        NST_uuid = resp_json["uuid"]
+        NST_name = resp_json["nstd"]["name"]
+        NST_author = resp_json["nstd"]["author"]
+        NST_usageState = resp_json["nstd"]["usageState"]
+        NST_onboardingState = resp_json["nstd"]["onboardingState"]
+        NST_operationalState = resp_json["nstd"]["operationalState"]
+        
+        self.assertEqual(NST_uuid, "096c26f9-6142-43d2-8521-57cea9e76c6c")
+        self.assertEqual(NST_name, "5gtango_NST_name")
+        self.assertEqual(NST_author, "5gTango")
+        self.assertEqual(NST_usageState, "NOT_IN_USE")
+        self.assertEqual(NST_onboardingState, "ENABLED")
+        self.assertEqual(NST_operationalState, "ENABLED")
 #    
 ##    def test_get_NST(self):
 ##        NetService_1_uuid = str(uuid.uuid4())
