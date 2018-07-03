d2424a0aea2d3773ca66463a2d19173adc3dedba
alemanyp <paprats8@gmail.com>
2018-07-03 14:07:04 +0200

    fixed file path in nsiapi/nstapi files

--- unit_tests/nsiapi.sh
+++ unit_tests/nsiapi.sh
@@ -3,4 +3,4 @@
 docker run -i \
 --rm=true \
 -v "$(pwd)/spec/reports/tng-slice-mngr:/code/log" \
-registry.sonata-nfv.eu:5000/tng-slice-mngr python /tng-slice-mngr/tests/unit/unit_nsi_test.py
+registry.sonata-nfv.eu:5000/tng-slice-mngr python /tng-slice-mngr/unit_tests/unit_nsi_test.py
