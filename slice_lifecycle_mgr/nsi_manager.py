ef86e920164f3631f7c03848f9d5d1005cf12206
alemanyp <paprats8@gmail.com>
2018-07-27 10:54:18 +0200

    missing variable in object creation for the provious commit solution

--- slice_lifecycle_mgr/nsi_manager.py
+++ slice_lifecycle_mgr/nsi_manager.py
@@ -139,7 +139,7 @@ def parseNewNSI(nst_json, nsi_json):
     netServInstance_Uuid = []                                                                               #values given when services are instantiated by the SP
     
     NSI=nsi.nsi_content(uuid_nsi, name, description, nstId, vendor, nstName, nstVersion, flavorId, 
-                  sapInfo, nsiState, instantiateTime, terminateTime, scaleTime, updateTime)
+                  sapInfo, nsiState, instantiateTime, terminateTime, scaleTime, updateTime, netServInstance_Uuid)
     return NSI
 
 def instantiateNetServices(NetServicesIDs):
