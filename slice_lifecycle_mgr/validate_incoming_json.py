db48cb07cdbd1f67e4b87ff065ceb7fca65323be
alemanyp <paprats8@gmail.com>
2019-03-21 16:20:34 +0100

    Removed comments in validate_incoming_json.py

--- slice_lifecycle_mgr/validate_incoming_json.py
+++ slice_lifecycle_mgr/validate_incoming_json.py
@@ -42,11 +42,8 @@ returnData = {}
 # Validation Functions
 def is_valid_uuid(uuid_to_test, version=4):
     """ Check if uuid_to_test is a valid UUID.
-    Parameters -----> uuid_to_test : str / version : {1, 2, 3, 4}
-    Returns --------> `True` if uuid_to_test is a valid UUID, otherwise `False`.
-    Examples
-    -- is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a') --> True
-    -- is_valid_uuid('c9bf9e58') --> False
+    Parameters --> uuid_to_test : str / version : {1, 2, 3, 4}
+    Returns    --> `True` if uuid_to_test is a valid UUID, otherwise `False`.
     """
     try:
         uuid_obj = UUID(uuid_to_test, version=version)
@@ -56,16 +53,6 @@ def is_valid_uuid(uuid_to_test, version=4):
 
 
 # CASE: Create NetSlice Template
-#Json_example:
-# {
-#   "name":"tango_NST",
-#   "version":"1.0",
-#   "author":"5gtango",
-#   "vendor":"5gTango",
-#   "nstNsdIds":[
-#     {"servname":"ser_1","nsdID": "c3305e70-4e6a-4741-91d2-672f00cdd437","slaID": "75ecf965-1afd-4330-b883-8672c2563785"},
-#   ]
-# }
 def validateCreateTemplate (jsonData):
   for item in jsonData['sliceServices']:
     if (is_valid_uuid (item['nsdID']) == True):
@@ -81,7 +68,6 @@ def validateCreateTemplate (jsonData):
       return (returnData, 400)
 
 # CASE: Create NetSlice instantiation
-# Json_example: {"name": "NSI_name", "description": "NSI_descriptor", "nstId": "26c540a8-1e70-4242-beef-5e77dfa05a41"}
 def validateCreateInstantiation (jsonData):
   if (is_valid_uuid(jsonData['nstId']) == True):
     returnData["missing_field"] = "Everything is OK!!"
@@ -92,7 +78,6 @@ def validateCreateInstantiation (jsonData):
     return (returnData, 400)
 
 # CASE: Terminate NetSlice Instantiation
-# Json_example: jsonData = {"terminateTime": <time>}
 # Possible values for <time> --> instant_termination: 0 / future termination: 2019-07-16T14:01:31.447547
 def validateTerminateInstantiation (jsonData):
   if (jsonData['terminateTime'] == 0 or jsonData['terminateTime'] == "0"):
